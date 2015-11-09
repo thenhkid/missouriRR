33ca4d98642033006dce2d39c7ab921c462ba7b9
Chad <chadmccue05@gmail.com>
2015-11-09 09:12:40 -0500

    - file upload type update

--- webapp/src/main/webapp/dspResources/js/forum/messages.js
+++ webapp/src/main/webapp/dspResources/js/forum/messages.js
@@ -105,7 +105,7 @@ jQuery(function ($) {
                    no_icon: 'ace-icon fa fa-cloud-upload',
                    droppable: false,
                    thumbnail: 'small',
-                   allowExt: ['pdf', 'txt', 'doc', 'docx', 'gif', 'png', 'jpg', 'jpeg', 'xls', 'xlsx', 'ppt'],
+                   allowExt: ['pdf', 'txt', 'doc', 'docx', 'gif', 'png', 'jpg', 'jpeg', 'xls', 'xlsx', 'ppt', 'csv', 'pptx', 'wma'],
                    before_remove: function () {
                        return true;
                    }
